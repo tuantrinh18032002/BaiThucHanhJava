59db6db84ba62dec50e5af306eeb9dd4ac87ceab
Your Nametuantrinh18032002 <tuana8978@gmail.com>
2022-02-20 12:31:47 +0700

    Create_Project_baithuchanh1802

---
+++ baithuchanh1802/src/App51.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class App51 {
+    public static void main (String[] args) {
+        int so =0;
+        for (so=0; so<5;so++){
+            System.out.println("so");
+        }
+    }
+
+}
